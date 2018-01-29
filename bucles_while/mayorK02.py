11ac37fa484597fcfab9d5308c733fd08bb61d28
kastillo3 <35691382+kastillo3@users.noreply.github.com>
2018-01-29 13:33:41 +0100

    Update mayorK02.py

--- bucles_while/mayorK02.py
+++ bucles_while/mayorK02.py
@@ -1,6 +1,6 @@
 # coding: utf-8
-numero1=input("introduce un numero")
+numero1=float(input("introduce un numero"))
 numero2=input("introduce un numero")
-while (numero1>numero2):
+while (numero2>numero1):
 	numero2=input("intentalo de nuevo")
 print("es mayor")
